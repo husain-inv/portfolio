06cc0f9ad1f8bbe6e133215077e672a4bc2d58f4
husain-inv <hoax.gila@gmail.com>
2021-09-22 10:51:16 +0700

    responsive

--- js/main.js
+++ js/main.js
@@ -306,3 +306,10 @@ function bodyScrollingToggle() {
     }
   }
 })();
+
+window.addEventListener("load", () => {
+  document.querySelector(".preloader").classList.add("fade-out");
+  setTimeout(() => {
+    document.querySelector(".preloader").style.display = "none";
+  }, 600);
+});
